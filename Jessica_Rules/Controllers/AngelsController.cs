18342c493e4c0195feccb34388dcd47a766c77bd
21647804 <21647804@dut4life.ac.za>
2019-09-16 11:51:04 +0200

    First commit

---
+++ Jessica_Rules/Controllers/AngelsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
+
+namespace Jessica_Rules.Controllers
+{
+    public class AngelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(FormPost form)
+        {
+            
+            //
+            return RedirectToAction("Edit", form);
+        }
+
+        public IActionResult Edit(FormPost form)
+        {
+            ViewData["Name"] = form.Name;
+            ViewData["Email"] = form.Email;
+            ViewData["Subject"] = form.Subject;
+            ViewData["Body"] = form.Body;
+
+            return View();
+        }
+    }
+
+    public class FormPost
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
